ed7467b85c1f79f4786c97aaa0f2d0352d63ee27
ryan3780 <45477679+ryan3780@users.noreply.github.com>
2019-02-16 20:12:57 +0900

    Update gretting.js

--- gretting.js
+++ gretting.js
@@ -1,44 +1,44 @@
-const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
-    gretting = document.querySelector(".js-grettings");
+// const form = document.querySelector(".js-form"),
+//     input = form.querySelector("input"),
+//     gretting = document.querySelector(".js-grettings");
 
-const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+// const USER_LS = "currentUser",
+//     SHOWING_CN = "showing";
 
-function saveName(text) {
-    localStorage.setItem(USER_LS, text);
-}
+// function saveName(text) {
+//     localStorage.setItem(USER_LS, text);
+// }
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const currentValue = input.value;
-    paintGretting(currentValue);
-    saveName(currentValue);
-}
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const currentValue = input.value;
+//     paintGretting(currentValue);
+//     saveName(currentValue);
+// }
 
-function askForName() {
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
-}
+// function askForName() {
+//     form.classList.add(SHOWING_CN);
+//     form.addEventListener("submit", handleSubmit);
+// }
 
-function paintGretting(text) {
-    form.classList.remove(SHOWING_CN);
-    gretting.classList.add(SHOWING_CN);
-    gretting.innerText = `${text}`;
-}
+// function paintGretting(text) {
+//     form.classList.remove(SHOWING_CN);
+//     gretting.classList.add(SHOWING_CN);
+//     gretting.innerText = `${text}`;
+// }
 
-function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
-    if (currentUser === null) {
-        askForName();
-    } else {
-        paintGretting(currentUser);
-    }
-}
+// function loadName() {
+//     const currentUser = localStorage.getItem(USER_LS);
+//     if (currentUser === null) {
+//         askForName();
+//     } else {
+//         paintGretting(currentUser);
+//     }
+// }
 
 
-function init() {
-    loadName();
-}
+// function init() {
+//     loadName();
+// }
 
-init();
+// init();
